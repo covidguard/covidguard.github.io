b6a94a5f27aaac9ec53d1318f230e0cb81dea8bb
Administrator <Administrator@ONE.local>
2020-03-20 17:28:09 +0000

    Update with the data of 20 March 2020

--- code/Analyse.m
+++ code/Analyse.m
@@ -1,5 +1,5 @@
-mkdir([WORKroot,'/slides/img/regioni']);
-mkdir([WORKroot,'/slides/img/province']);
+% mkdir([WORKroot,'/slides/img/regioni']);
+% mkdir([WORKroot,'/slides/img/province']);
 % mkdir('slides');
 % mkdir('slides/img');
 % mkdir('slides/img/province');
@@ -8,9 +8,10 @@ mkdir([WORKroot,'/slides/img/province']);
 
 %% download json from server
 %% -------------------------
-delete(sprintf('%s/JSON/*.json',WORKroot))
+WORKroot = sprintf('C:/Temp/Repo/covidguard');
+delete(sprintf('%s/_json/*.json',WORKroot))
 
-WORKroot = sprintf('C:/Temp/COVID');
+
 serverAddress = 'https://raw.githubusercontent.com';
 
 command = sprintf('wget --no-check-certificate %s/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-province.json', serverAddress);
@@ -19,20 +20,21 @@ system(command);
 command = sprintf('wget --no-check-certificate %s/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-regioni.json', serverAddress);
 system(command);
 
-movefile('dpc-covid19-ita-province.json',sprintf('%s/JSON',WORKroot),'f');
-movefile('dpc-covid19-ita-regioni.json',sprintf('%s/JSON',WORKroot)),'f';
+movefile('dpc-covid19-ita-province.json',sprintf('%s/_json',WORKroot),'f');
+movefile('dpc-covid19-ita-regioni.json',sprintf('%s/_json',WORKroot)),'f';
 
 
-
-
-
+%% load population
+%% ---------------
+filename = 'C:/Temp/COVID/json/popolazione_province.txt';
+[pop.id, pop.name, pop.number, pop.perc, pop.superf, pop.numCom, pop.sigla]=textread(filename,'%d%s%d%f%d%d%s','delimiter',';');
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Regioni
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-filename = sprintf('%s/JSON/dpc-covid19-ita-regioni.json',WORKroot);
+filename = sprintf('%s/_json/dpc-covid19-ita-regioni.json',WORKroot);
 fid       = fopen(filename, 'rt');
 file_scan = textscan(fid, '%s', 'delimiter', '\n', 'endOfLine', '\r\n', 'whitespace', '');
 fclose(fid);
@@ -107,6 +109,7 @@ for reg=1:size(regioni_tot,1)
     ylabel('Numero casi', 'FontName', 'Verdana', 'FontWeight', 'Bold','FontSize',8);
     set(code_axe, 'Xlim', [time_num(1), time_num(end)]);
     datetick('x', datetickFormat, 'keeplimits') ;
+    set(gca,'XTickLabelRotation',53,'FontSize',7);
     
     t_lim=ylim;
     if t_lim(2)<100
@@ -129,7 +132,7 @@ for reg=1:size(regioni_tot,1)
     
     
 % %     cd([WORKroot,'/assets/img/regioni']);
-    print(gcf, '-dpng', [WORKroot,'/assets/img/regioni/reg_',regione, '_cumulati.PNG']);
+    print(gcf, '-dpng', [WORKroot,'/slides/img/regioni/reg_',regione, '_cumulati.PNG']);
     close(gcf);
 %     cd([WORKroot,'/code']);
     
@@ -186,6 +189,7 @@ for reg=1:size(regioni_tot,1)
     ylabel('Numero casi', 'FontName', 'Verdana', 'FontWeight', 'Bold','FontSize',8);
     set(code_axe, 'Xlim', [time_num(2), time_num(end)]);
     datetick('x', datetickFormat, 'keeplimits') ;
+    set(gca,'XTickLabelRotation',53,'FontSize',7);
     
     t_lim=ylim;
     if t_lim(2)<100
@@ -206,7 +210,7 @@ for reg=1:size(regioni_tot,1)
         'FitBoxToText','off',...
         'LineStyle','none',...
         'Color',[0 0 0]);
-    print(gcf, '-dpng', [WORKroot,'/assets/img/regioni/reg_',regione, '_giornalieri.PNG']);
+    print(gcf, '-dpng', [WORKroot,'/slides/img/regioni/reg_',regione, '_giornalieri.PNG']);
     close(gcf);
     catch
     end
@@ -232,10 +236,9 @@ end
 
 
 
-
 %%%%%%%%%%%%%%%%%%
 %% Province
-filename = sprintf('%s/JSON/dpc-covid19-ita-province.json',WORKroot);
+filename = sprintf('%s/_json/dpc-covid19-ita-province.json',WORKroot);
 fid       = fopen(filename, 'rt');
 file_scan = textscan(fid, '%s', 'delimiter', '\n', 'endOfLine', '\r\n', 'whitespace', '');
 fclose(fid);
@@ -256,13 +259,20 @@ Regione_lista = unique(dataReg.denominazione_regione);
 
 % RegioneTot={'Padova','Brescia'}
 
+normalizza_per_popolazione=0;
+
 for reg = 1:size(Regione_lista)
+    
     idx_reg=find(strcmp(dataReg.denominazione_regione,cell(Regione_lista(reg,:))));
     sigla_prov=dataReg.sigla_provincia(idx_reg);
     [RegioneTot, ixs]= unique(dataReg.denominazione_provincia(idx_reg));
     sigla_prov=sigla_prov(ixs);
     [RegioneTot, ixs]=setdiff(RegioneTot,cellstr('In fase di definizione/aggiornamento'));
     sigla_prov=sigla_prov(ixs);
+    
+    % find population
+    [~,idx_pop] = intersect(pop.sigla,cell(sigla_prov));
+
 %     RegioneTot={'Como','Lecco','Milano','Bergamo','Varese','Lodi','Monza e della Brianza'}';
 %     RegioneTot={'Como','Bergamo','Brescia','Lecco'}'
     try
@@ -282,10 +292,18 @@ for reg = 1:size(Regione_lista)
         regione = char(RegioneTot(h));
         index = find(strcmp(dataReg.denominazione_provincia,cellstr(regione))&strcmp(dataReg.denominazione_regione,cellstr(Regione_lista(reg,:))));
         time_num = fix(datenum(dataReg.data(index)));
-        try
-            b(h)=plot(time_num,dataReg.totale_casi(index,1),'-','LineWidth', 2.0,  'Color', Cmap.getColor(h, size(RegioneTot,1)));
-        catch
-            b(h)=plot(time_num,str2double(dataReg.totale_casi(index,1)),'-','LineWidth', 2.0,  'Color', Cmap.getColor(h, size(RegioneTot,1)));    
+        if normalizza_per_popolazione==1
+            try
+                b(h)=plot(time_num,(dataReg.totale_casi(index,1)/pop.number(idx_pop(h)))*1000,'-','LineWidth', 2.0,  'Color', Cmap.getColor(h, size(RegioneTot,1)));
+            catch
+                b(h)=plot(time_num,(str2double(dataReg.totale_casi(index,1))/pop.number(idx_pop(h)))*1000,'-','LineWidth', 2.0,  'Color', Cmap.getColor(h, size(RegioneTot,1)));
+            end
+        else
+            try
+                b(h)=plot(time_num,dataReg.totale_casi(index,1),'-','LineWidth', 2.0,  'Color', Cmap.getColor(h, size(RegioneTot,1)));
+            catch
+                b(h)=plot(time_num,str2double(dataReg.totale_casi(index,1)),'-','LineWidth', 2.0,  'Color', Cmap.getColor(h, size(RegioneTot,1)));
+            end
         end
         regione_leg=regione;
         regione_leg(strfind(regione_leg,''''))=' ';
@@ -296,13 +314,19 @@ for reg = 1:size(Regione_lista)
     code_axe = get(id_f, 'CurrentAxes');
     set(code_axe, 'FontName', 'Verdana');
     set(code_axe, 'FontSize', 8);
-    ylabel('Numero casi', 'FontName', 'Verdana', 'FontWeight', 'Bold','FontSize',8);
+    if normalizza_per_popolazione==1
+        ylabel('Numero casi x1000 abitanti', 'FontName', 'Verdana', 'FontWeight', 'Bold','FontSize',8);
+    else
+        ylabel('Numero casi', 'FontName', 'Verdana', 'FontWeight', 'Bold','FontSize',8);
+        t_lim=ylim;
+            if t_lim(2)<100
+                ylim([t_lim(1) 100]);
+            end
+    end
     set(code_axe, 'Xlim', [time_num(1), time_num(end)]);
     datetick('x', datetickFormat, 'keeplimits') ;
-    t_lim=ylim;
-    if t_lim(2)<100
-        ylim([t_lim(1) 100]);
-    end
+    set(gca,'XTickLabelRotation',53,'FontSize',7);
+
     eval(string_legend)
     
     % l=legend([b],'Totale Casi');
@@ -317,8 +341,12 @@ for reg = 1:size(Regione_lista)
         'FitBoxToText','off',...
         'LineStyle','none',...
         'Color',[0 0 0]);
-    
-    print(gcf, '-dpng', [WORKroot,'/assets/img/province/Province_',char(Regione_lista(reg)) ,'_casiTotaliCumulati.PNG']);
+    if normalizza_per_popolazione==1
+        print(gcf, '-dpng', [WORKroot,'/slides/img/province/Province_norm',char(Regione_lista(reg)) ,'_casiTotaliCumulati.PNG']);       
+    else
+        print(gcf, '-dpng', [WORKroot,'/slides/img/province/Province_',char(Regione_lista(reg)) ,'_casiTotaliCumulati.PNG']);       
+    end
+
     close(gcf);
     
     
@@ -342,26 +370,35 @@ for reg = 1:size(Regione_lista)
     
     for h=1:size(RegioneTot,1)
         regione = char(RegioneTot(h));
-%         index = find(strcmp(dataReg.denominazione_provincia,cellstr(regione))&strcmp(dataReg.denominazione_regione,cellstr(Regione_lista(reg,:))));
+        %         index = find(strcmp(dataReg.denominazione_provincia,cellstr(regione))&strcmp(dataReg.denominazione_regione,cellstr(Regione_lista(reg,:))));
         index = find(strcmp(dataReg.denominazione_provincia,cellstr(regione)));
         time_num = fix(datenum(dataReg.data(index)));
-        try
-            b(h)=plot(time_num(2:end),diff(dataReg.totale_casi(index,1)),'-','LineWidth', 2.0,  'Color', Cmap.getColor(h, size(RegioneTot,1)));
-        catch
-            keyboard
-            bbbb=str2double(dataReg.totale_casi(index,1));
-            b(h)=plot(time_num(2:end),diff(bbbb),'-','LineWidth', 2.0,  'Color', Cmap.getColor(h, size(RegioneTot,1)));
-        end
+        
+%         if normalizza_per_popolazione==1
+%             try
+%                 b(h)=plot(time_num(2:end),diff(dataReg.totale_casi(index,1))/pop.number(idx_pop(h))*1000,'-','LineWidth', 2.0,  'Color', Cmap.getColor(h, size(RegioneTot,1)));
+%             catch
+%                 bbbb=str2double(dataReg.totale_casi(index,1));
+%                 b(h)=plot(time_num(2:end),diff(bbbb)/pop.number(idx_pop(h))*1000,'-','LineWidth', 2.0,  'Color', Cmap.getColor(h, size(RegioneTot,1)));
+%             end
+%         else
+            try
+                b(h)=plot(time_num(2:end),diff(dataReg.totale_casi(index,1)),'-','LineWidth', 2.0,  'Color', Cmap.getColor(h, size(RegioneTot,1)));
+            catch
+                bbbb=str2double(dataReg.totale_casi(index,1));
+                b(h)=plot(time_num(2:end),diff(bbbb),'-','LineWidth', 2.0,  'Color', Cmap.getColor(h, size(RegioneTot,1)));
+            end
+%         end
         regione_leg=regione;
         regione_leg(strfind(regione_leg,''''))=' ';
         string_legend=sprintf('%s,''%s''',string_legend,regione_leg);
         code_axe = get(id_f, 'CurrentAxes');
         set(code_axe, 'Xlim', [time_num(2), time_num(end)]);
         
-
+        
         try
-        sf=diff((dataReg.totale_casi(index,1)));
-        catch
+            sf=diff((dataReg.totale_casi(index,1)));
+            catch
             sf=diff(str2double(dataReg.totale_casi(index,1)));
         end
         testo.sigla(h,:)=char(sigla_prov(h));
@@ -384,19 +421,14 @@ for reg = 1:size(Regione_lista)
           text(time_num(end)+((time_num(end)-time_num(1)))*0.01, (testo.pos(indext(ll),2)),...
             ['-> ',testo.sigla(indext(ll),:)], 'HorizontalAlignment','left','FontSize',7','Color',[0 0 0]) ;    
         end
-
-        
     end
-    
-    
-    
-    
     
     set(code_axe, 'FontName', 'Verdana');
     set(code_axe, 'FontSize', 8);
     ylabel('Numero casi', 'FontName', 'Verdana', 'FontWeight', 'Bold','FontSize',8);
     set(code_axe, 'Xlim', [time_num(2), time_num(end)]);
     datetick('x', datetickFormat, 'keeplimits') ;
+    set(gca,'XTickLabelRotation',53,'FontSize',7);
     t_lim=ylim;
     if t_lim(2)<100
         ylim([t_lim(1) 100]);
@@ -414,7 +446,7 @@ for reg = 1:size(Regione_lista)
         'FitBoxToText','off',...
         'LineStyle','none',...
         'Color',[0 0 0]);
-    print(gcf, '-dpng', [WORKroot,'/assets/img/province/Province_',char(Regione_lista(reg)) ,'_casiTotaliGiornalieri.PNG']);
+    print(gcf, '-dpng', [WORKroot,'/slides/img/province/Province_',char(Regione_lista(reg)) ,'_casiTotaliGiornalieri.PNG']);
     close(gcf);
     catch
     end
