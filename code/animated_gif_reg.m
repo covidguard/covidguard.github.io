fd5b765b863b92610e808adf8d5b01de208efce9
Andrea <gatti.andrea@gmail.com>
2020-03-23 19:00:44 +0100

    Update gif generator

--- code/animated_gif_reg.m
+++ code/animated_gif_reg.m
@@ -43,10 +43,14 @@ colors={[0 0.4470 0.7410],[0.8500 0.3250 0.0980],[0.9290 0.6940 0.1250],[0.4940 
 x_data_int=[];
 y_data_int=[];
 %interpolation piecewise
+tmp = x_data;
+tmp(tmp == 0) = 1;
+step = 0.2;
 for i = 1:size(x_data,1)-1
-    [~, ~, ~, x_data_int(:,i)] = splinerMat(1:size(x_data,1),x_data(:,i),4,0,1:0.2:size(x_data,1));
-    [~, ~, ~, y_data_int(:,i)] = splinerMat(1:size(x_data,1),y_data(:,i),4,0,1:0.2:size(x_data,1));
+    [~, ~, ~, x_data_int(:,i)] = splinerMat(1:size(x_data,1),tmp(:,i),3,0,1:step:size(x_data,1));
+    [~, ~, ~, y_data_int(:,i)] = splinerMat(1:size(x_data,1),y_data(:,i),4,0,1:step:size(x_data,1));
 end
+clear tmp
 
 % 
 % 
@@ -60,38 +64,55 @@ end
 %     end
 %     
 % end
+x_data_int(x_data_int <= 0) = 0;
+y_data_int(y_data_int <= 1) = nan;
+days = (datenum(unique(data.dataReg.data)));
+days = floor(interp1q((1:size(days,1))', days, 6 + (1:step:size(x_data,1))'));
 
+%%
 x_data=x_data_int;
 y_data=y_data_int;
+
+% Init labels
+l=1;
+x = x_data(1,:);
+y = y_data(1,:);
+clear lbl;
+hdate = text(-0.08, 1.5, datestr(days(1), 'dd mmm'), 'Color', [0.5 0.5 0.5],'fontsize',20,'FontWeight','bold');
+hold on;
+for q=1:length(x)
+    %plot(x(q)',y(q)',markers{l},'w')
+    if q==12 || q==13
+        lbl(q) = text(x(q),y(q), upper(regioni_tot{q}(6:8)),'Color', colors{l},'fontsize',14,'FontWeight','bold');
+    else
+        lbl(q) = text(x(q),y(q), upper(regioni_tot{q}(1:3)),'Color', colors{l},'fontsize',14);
+        l=l+1;
+        if l==7
+            l=1;
+        end
+    end
+end
+grid on
+text(0.6, 260000, {'alto tasso di crescita e','  alto numero di casi'},'Color','k','fontsize',14)
+text(0, 260000, {'epidemia sotto','   controllo'},'Color','k','fontsize',14)
+ylabel('Numero di casi totali')
+xlabel('Incremento settimanale di casi totali')
+set(gcf,'color','w');
+
+fh = gcf; beautifyFig(fh);
 
 for n = 1:size(x_data,1)
     % Draw plot for y = x.^n
     x = x_data(n,:);
     y = y_data(n,:);
     hold off
-    l=1;
-    delete(findall(gcf,'type','text'))
+    hdate.String = datestr(days(n), 'dd mmm');
     for q=1:length(x)
         %plot(x(q)',y(q)',markers{l},'w')
-        if q==12 || q==13
-            text(x(q),y(q),regioni_tot{q}(6:8),'Color', colors{l},'fontsize',8,'FontWeight','bold')
-        else
-            text(x(q),y(q),regioni_tot{q}(1:3),'Color', colors{l},'fontsize',8)
-            hold on
-            l=l+1;
-            if l==7
-                l=1;
-            end
-        end
+        lbl(q).Position(1:2) = [x(q), y(q)];
     end
     %legend(regioni_tot,'Location', 'NorthEastOutside'
-    grid on
     %title()
-    text(0.6, 260000, {'alto tasso di crescita e','  alto numero di casi'},'Color','k','fontsize',10)
-    text(0, 260000, {'epidemia sotto','   controllo'},'Color','k','fontsize',10)
-    ylabel('Numero di casi totali')
-    xlabel('incremento settimanale di casi totali')
-    set(gcf,'color','w');
     %title('')
     drawnow
     % Capture the plot as an image
@@ -109,4 +130,169 @@ for n = 1:size(x_data,1)
 end
 close all
 
+end
+
+function beautifyFig(fig_handle)
+    % Change font size / type colors of a figure
+    %
+    % INPUT:
+    %   fig_handle  figure handler (e.g. gcf)
+    %   color_mode  two modes are supported:
+    %               'light'     classic Light mode
+    %
+    % SYNTAX:
+    %   beautifyFig(fig_handle)
+    FONT = 'Open Sans';
+    %FONT = 'Helvetica';
+    if nargin == 0 || isempty(fig_handle)
+        fig_handle = gcf;
+    end
+    color_mode = 'light';
+    ax_list = findall(fig_handle,'type','axes');
+    for ax = ax_list(:)'
+        ax.FontName = 'Open Sans';
+        ax.FontWeight = 'bold';
+    end
+    set(fig_handle, ...
+        'DefaultFigureColor', 'w', ...
+        'DefaultAxesLineWidth', 0.5, ...
+        'DefaultAxesXColor', 'k', ...
+        'DefaultAxesYColor', 'k', ...
+        'DefaultAxesFontUnits', 'points', ...
+        'DefaultAxesFontSize', 12, ...
+        'DefaultAxesFontName', FONT, ...
+        'DefaultLineLineWidth', 1, ...
+        'DefaultTextFontUnits', 'Points', ...
+        'DefaultTextFontSize', 18, ...
+        'DefaultTextFontName', FONT, ...
+        'DefaultTextFontWeight', 'bold', ...
+        'DefaultAxesBox', 'off', ...
+        'DefaultAxesTickLength', [0.02 0.025]);
+    
+    set(fig_handle, 'DefaultAxesTickDir', 'out');
+    set(fig_handle, 'DefaultAxesTickDirMode', 'manual');
+    
+    
+    ui_list = findall(fig_handle, 'Type', 'uicontrol');
+    for ui = ui_list(:)'
+        if ~ischar(ui.BackgroundColor) && all(ui.BackgroundColor > 0.5)
+            ui.FontName = FONT;
+            ui.FontSize = iif(ui.FontSize == 12, 16, 18);
+        end
+    end
+    
+    cb_list = findall(fig_handle, 'Type', 'colorbar');
+    for cb = cb_list(:)'
+        cb.FontName = FONT;
+        cb.FontSize = 18;
+        cbt_list = findall(cb.UserData, 'Type', 'text');
+        for cbt = cbt_list(:)'
+            cbt.FontName = FONT;
+            cbt.FontSize = 16;
+        end
+    end
+    
+    ax_list = findall(fig_handle,'type','axes');
+    for ax = ax_list(:)'
+        ax.FontName = FONT;
+        ax.FontSize = 14;
+        text_label = findall(ax, 'Type', 'text');
+        for txt = text_label(:)'
+            % If the text have the same color of the background change it accordingly
+            txt.FontName = FONT;
+        end
+    end
+    text_label = findall(gcf,'Tag', 'm_grid_xticklabel');
+    for txt = text_label(:)'
+        txt.FontName = FONT;
+        txt.FontSize = iif(txt.FontSize == 12, 13, 15);
+    end
+    text_label = findall(gcf,'Tag', 'm_grid_yticklabel');
+    for txt = text_label(:)'
+        txt.FontName = FONT;
+        txt.FontSize = iif(txt.FontSize == 12, 13, 15);
+    end
+    text_label = findall(gcf,'Tag', 'm_ruler_label');
+    for txt = text_label(:)'
+        txt.FontName = FONT;
+        txt.FontSize = iif(txt.FontSize == 12, 13, 15);
+    end
+    legend = findall(gcf, 'type', 'legend');
+    for lg = legend(:)'
+        lg.FontName = FONT;
+        lg.FontSize = 11;
+    end
+    
+    if strcmp(color_mode, 'light') % ------------------------------------------------------------------- LIGHT
+        fig_handle.Color = [1 1 1];
+        box_list = findall(fig_handle, 'Type', 'uicontainer');
+        for box = box_list(:)'
+            if ~ischar(box.BackgroundColor) && all(box.BackgroundColor < 0.5)
+                box.BackgroundColor = 1 - box.BackgroundColor;
+            end
+        end
+        ui_list = findall(fig_handle, 'Type', 'uicontrol');
+        for ui = ui_list(:)'
+            if ~ischar(ui.BackgroundColor) && all(ui.BackgroundColor < 0.5)
+                ui.ForegroundColor = 1 - ui.ForegroundColor;
+                ui.BackgroundColor = 1 - ui.BackgroundColor;
+            end
+        end
+        
+        cb_list = findall(fig_handle, 'Type', 'colorbar');
+        for cb = cb_list(:)'
+            cb.Color = 1-[0.8 0.8 0.8];
+            cbt_list = findall(cb.UserData, 'Type', 'text');
+            for cbt = cbt_list(:)'
+                cbt.Color = 1-[0.8 0.8 0.8];
+            end
+        end
+        ax_list = findall(fig_handle,'type','axes');
+        for ax = ax_list(:)'
+            ax.Color = [1 1 1];
+            ax.Title.Color = 1-[1 1 1];
+            ax.XLabel.Color = 1-[0.8 0.8 0.8];
+            ax.YLabel.Color = 1-[0.8 0.8 0.8];
+            ax.ZLabel.Color = 1-[0.8 0.8 0.8];
+            ax.XColor = 1-[0.8 0.8 0.8];
+            ax.YColor = 1-[0.8 0.8 0.8];
+            ax.ZColor = 1-[0.8 0.8 0.8];
+            text_label = findall(ax, 'Type', 'text');
+            for txt = text_label(:)'
+                % If the text have the same color of the background change it accordingly
+                if isnumeric(txt.BackgroundColor) && isnumeric(txt.Color) && sum(abs(txt.BackgroundColor - txt.Color)) == 0
+                    if ~ischar(txt.BackgroundColor) && all(txt.BackgroundColor < 0.5)
+                        txt.Color = 1 - txt.Color;
+                        txt.BackgroundColor = 1 - txt.BackgroundColor;
+                    end
+                else
+                    if ~ischar(txt.Color) && all(txt.Color > 0.5)
+                        txt.Color = 1 - txt.Color;
+                    end
+                    if ~ischar(txt.BackgroundColor) && all(txt.BackgroundColor < 0.5)
+                        txt.BackgroundColor = 1 - txt.BackgroundColor;
+                    end
+                end
+            end
+        end
+        text_label = findall(gcf,'Tag', 'm_grid_xticklabel');
+        for txt = text_label(:)'
+            txt.Color = 1-[0.8 0.8 0.8];
+        end
+        text_label = findall(gcf,'Tag', 'm_grid_yticklabel');
+        for txt = text_label(:)'
+            txt.Color = 1-[0.8 0.8 0.8];
+        end
+        text_label = findall(gcf,'Tag', 'm_ruler_label');
+        for txt = text_label(:)'
+            txt.Color = 1-[0.8 0.8 0.8];
+        end
+        legend = findall(gcf, 'type', 'legend');
+        for lg = legend(:)'
+            lg.Color = [1 1 1];
+            lg.Title.Color = [0 0 0];
+            lg.TextColor = 1-[0.8 0.8 0.8];
+            lg.EdgeColor = [0.5 0.5 0.5];
+        end
+    end
 end
