9a138dfc2db6da570645be492fcf4aff5cb61771
Andrea Gatti <gatti.andrea@gmail.com>
2020-08-30 18:22:59 +0000

    update 30/08

--- code/Analyse_gompertz.m
+++ code/Analyse_gompertz.m
@@ -59,7 +59,7 @@ for i=1:size(file_scan,1)
     json_oneRaw(1+(i-1)*size(file_scan,2):i*size(file_scan,2))=file_scan(i,:);
     %     json_oneRaw=sprintf('%s%s',json_oneRaw,file_scan(i,:));
 end
-dataReg = decodeJSON(json_oneRaw);
+dataReg = decodeJSON1(json_oneRaw);
 dataReg.dataa = char(dataReg.data);
 dataReg.dataa(:,11)=' ';
 dataReg.data=cellstr(dataReg.dataa);
@@ -85,7 +85,7 @@ for i=1:size(file_scan,1)
     %     json_oneRaw=sprintf('%s%s',json_oneRaw,file_scan(i,:));
 end
 
-dataProv = decodeJSON(json_oneRaw);
+dataProv = decodeJSON1(json_oneRaw);
 dataProv.dataa = char(dataProv.data);
 dataProv.dataa(:,11)=' ';
 dataProv.data=cellstr(dataProv.dataa);
@@ -2160,158 +2160,158 @@ for reg = 1:size(regioni_tot,1);
     
     t=time_num(2:end);
     
-
-
-
-y1=diff(data_casiPositivi);
-y2=diff(data_deceduti);
-% y2=data_terapie;
-
-l1='Casi positivi gionalieri';
-l2='Deceduti giornalieri';
-% l2='Terapie giornalieri';
-
-[a1_1] = splinerMat(t,y1,7);
-% figure;hold on
-% plot(t,y1,'.b');
-% plot(t,a1_1,'-r');
-
-
-[a1_2] = splinerMat(t,y2,7);
-% figure;hold on
-% plot(t,y2,'.b');
-% plot(t,a1_2,'-r');
-
-t_1_1=t;
-t_1_2=t;
-
-corrcoeff_tot=[];
-for d=0:16
-    a=corrcoef(a1_1(1:end-d),a1_2(d+1:end));
-    corrcoeff_tot(d+1)=a(1,2);
-end
-[coerrcoeff_max, idx]=max(corrcoeff_tot);
-offset_dec=idx-1;
-
-
-figure;
-id_f = gcf;
-set(gcf, 'Name', sprintf('%s: correlazione temporale',regione));
-% tt=title(sprintf([regione ': correlazione nuovi casi/deceduti giornalieri\\fontsize{5}\n ']),'fontsize',9)
-annotation(gcf,'textbox',...
-    [0.222148098732488 0.890266851563407 0.537691794529685 0.0630182421227199],...
-    'String',[regione ': correlazione nuovi casi/deceduti giornalieri'],...
-    'LineStyle','none',...
-    'HorizontalAlignment','center',...
-    'FontSize',10,...
-    'FontName','Verdana',...
-    'FitBoxToText','off');
-
-set(gcf,'NumberTitle','Off');
-set(gcf,'Position',[26 79 967 603]);
-grid on
-hold on
-
-[ax,a,b] = plotxx(t,y1,t,y2);
-hold on
-
-ax1 = ax(1);
-ax2 = ax(2);
-
-set(gcf,'CurrentAxes',ax1)
-c=plot(t_1_1,a1_1,'-r');
-
-set(gcf,'CurrentAxes',ax2)
-d=plot(t_1_2,a1_2,'-b');
-
-%
-% a=plot(t,data_casiPositivi,'-ob','LineWidth', 2.0,'color',[0 0.200000002980232 0.600000023841858]); set(a,'markersize',6,'MarkerFaceColor',[0 0.447058826684952 0.74117648601532],'Color',[0 0.447058826684952 0.74117648601532]);
-% b=plot(t-offset_dec,data_deceduti,'-ob','LineWidth', 2.0,'color',[1 0.200000002980232 0.600000023841858],'Parent',ax2); set(b,'markersize',6,'MarkerFaceColor',[0.850980401039124 0.325490206480026 0.0980392172932625],'Color',[0.850980401039124 0.325490206480026 0.0980392172932625]);
-
-
-if ismac
-    font_size = 9;
-else
-    font_size = 6.5;
-end
-
-set(a,'marker','o','markersize',3,'MarkerFaceColor',[0 0.447058826684952 0.74117648601532],'Color',[0 0.447058826684952 0.74117648601532],'LineWidth', 0.5);
-set(b,'marker','o','markersize',3,'MarkerFaceColor',[0.850980401039124 0.325490206480026 0.0980392172932625],'Color',[0.850980401039124 0.325490206480026 0.0980392172932625],'LineWidth', 0.5);
-set(c,'Color',[0 0.447058826684952 0.74117648601532],'LineWidth', 2.0);
-set(d,'Color',[0.850980401039124 0.325490206480026 0.0980392172932625],'LineWidth', 2.0);
-
-
-
-set(gcf,'CurrentAxes',ax1)
-ylabel(l1, 'FontName', 'Verdana', 'FontWeight', 'Bold','FontSize',8);
-set(ax1, 'FontName', 'Verdana');
-set(ax1, 'FontSize', font_size);
-
-% set(ax1,'ylim',([0,80]));
-ax1.XTick = datenum(time_num(1:2:end));
-datetick('x', 'dd mmm', 'keepticks') ;
-set(ax1,'xlim',([t(1),t(end)]));
-set(ax1,'XTickLabelRotation',53,'FontSize',6.5);
-set(ax1,'Xcolor',[0 0.447058826684952 0.74117648601532]);
-set(ax1,'Ycolor',[0 0.447058826684952 0.74117648601532]);
-ylims=get(ax1,'ylim');
-ylim(ax1,[0 ylims(2)]);
-
-
-set(gcf,'CurrentAxes',ax2)
-ylabel(l2, 'FontName', 'Verdana', 'FontWeight', 'Bold','FontSize',8);
-set(ax2, 'FontName', 'Verdana');
-set(ax2, 'FontSize', font_size);
-% set(ax2,'xlim',([t(1)+offset_dec-1,t(end)+offset_dec-1]));
-% set(ax2,'xlim',([t(1)+offset_dec-1,t(end)+offset_dec-1]));
-% ax2.XTick = t(1:2:end)+offset_dec-1
-% set(ax2,'ylim',([0,80]));
-ax2.XTick = datenum(time_num(1:2:end))+offset_dec;
-set(ax2,'xlim',([t(1)+offset_dec,t(end)+offset_dec]));
-ax2.XTickLabel = datestr(datenum(time_num(1:2:end))+offset_dec);
-datetick('x', 'dd mmm', 'keepticks') ;
-
-set(ax2,'XTickLabelRotation',53,'FontSize',6.5);
-set(ax2,'Xcolor',[0.850980401039124 0.325490206480026 0.0980392172932625]);
-set(ax2,'Ycolor',[0.850980401039124 0.325490206480026 0.0980392172932625]);
-ylims=get(ax2,'ylim');
-ylim(ax2,[0 ylims(2)]);
-l=legend([a,b],l1,l2);
-
-set(l,'Location','northwest')
-% overlap copyright info
-datestr_now = datestr(now);
-annotation(gcf,'textbox',[0.72342 0.00000 0.2381 0.04638],...
-    'String',{['Fonte: https://github.com/pcm-dpc']},...
-    'HorizontalAlignment','center',...
-    'FontSize',6,...
-    'FontName','Verdana',...
-    'FitBoxToText','off',...
-    'LineStyle','none',...
-    'Color',[0 0 0]);
-
-annotation(gcf,'textbox',...
-    [0.125695077559464 0.00165837479270315 0.238100000000001 0.04638],...
-    'String',{'https://covidguard.github.io/#covid-19-italia'},...
-    'LineStyle','none',...
-    'HorizontalAlignment','left',...
-    'FontSize',6,...
-    'FontName','Verdana',...
-    'FitBoxToText','off');
-
-annotation(gcf,'textbox',...
-    [0.328498902750058 0.735693681496092 0.537691794529686 0.0630182421227199],...
-    'String',['Offset: ', num2str(offset_dec), ' giorni'],...
-    'LineStyle','none',...
-    'HorizontalAlignment','right',...
-    'FontName','Verdana',...
-    'FitBoxToText','off');
-
-
-
-print(gcf, '-dpng', [WORKroot,'/slides/img/regioni/reg_correlazioneCasiDeceduti_v2_spline_', num2str(reg) ,'_', regione,'.PNG']);
-close(gcf);
-
+    
+    
+    
+    y1=diff(data_casiPositivi);
+    y2=diff(data_deceduti);
+    % y2=data_terapie;
+    
+    l1='Casi positivi gionalieri';
+    l2='Deceduti giornalieri';
+    % l2='Terapie giornalieri';
+    
+    [a1_1] = splinerMat(t,y1,7);
+    % figure;hold on
+    % plot(t,y1,'.b');
+    % plot(t,a1_1,'-r');
+    
+    
+    [a1_2] = splinerMat(t,y2,7);
+    % figure;hold on
+    % plot(t,y2,'.b');
+    % plot(t,a1_2,'-r');
+    
+    t_1_1=t;
+    t_1_2=t;
+    
+    corrcoeff_tot=[];
+    for d=0:16
+        a=corrcoef(a1_1(1:end-d),a1_2(d+1:end));
+        corrcoeff_tot(d+1)=a(1,2);
+    end
+    [coerrcoeff_max, idx]=max(corrcoeff_tot);
+    offset_dec=idx-1;
+    
+    
+    figure;
+    id_f = gcf;
+    set(gcf, 'Name', sprintf('%s: correlazione temporale',regione));
+    % tt=title(sprintf([regione ': correlazione nuovi casi/deceduti giornalieri\\fontsize{5}\n ']),'fontsize',9)
+    annotation(gcf,'textbox',...
+        [0.222148098732488 0.890266851563407 0.537691794529685 0.0630182421227199],...
+        'String',[regione ': correlazione nuovi casi/deceduti giornalieri'],...
+        'LineStyle','none',...
+        'HorizontalAlignment','center',...
+        'FontSize',10,...
+        'FontName','Verdana',...
+        'FitBoxToText','off');
+    
+    set(gcf,'NumberTitle','Off');
+    set(gcf,'Position',[26 79 967 603]);
+    grid on
+    hold on
+    
+    [ax,a,b] = plotxx(t,y1,t,y2);
+    hold on
+    
+    ax1 = ax(1);
+    ax2 = ax(2);
+    
+    set(gcf,'CurrentAxes',ax1)
+    c=plot(t_1_1,a1_1,'-r');
+    
+    set(gcf,'CurrentAxes',ax2)
+    d=plot(t_1_2,a1_2,'-b');
+    
+    %
+    % a=plot(t,data_casiPositivi,'-ob','LineWidth', 2.0,'color',[0 0.200000002980232 0.600000023841858]); set(a,'markersize',6,'MarkerFaceColor',[0 0.447058826684952 0.74117648601532],'Color',[0 0.447058826684952 0.74117648601532]);
+    % b=plot(t-offset_dec,data_deceduti,'-ob','LineWidth', 2.0,'color',[1 0.200000002980232 0.600000023841858],'Parent',ax2); set(b,'markersize',6,'MarkerFaceColor',[0.850980401039124 0.325490206480026 0.0980392172932625],'Color',[0.850980401039124 0.325490206480026 0.0980392172932625]);
+    
+    
+    if ismac
+        font_size = 9;
+    else
+        font_size = 6.5;
+    end
+    
+    set(a,'marker','o','markersize',3,'MarkerFaceColor',[0 0.447058826684952 0.74117648601532],'Color',[0 0.447058826684952 0.74117648601532],'LineWidth', 0.5);
+    set(b,'marker','o','markersize',3,'MarkerFaceColor',[0.850980401039124 0.325490206480026 0.0980392172932625],'Color',[0.850980401039124 0.325490206480026 0.0980392172932625],'LineWidth', 0.5);
+    set(c,'Color',[0 0.447058826684952 0.74117648601532],'LineWidth', 2.0);
+    set(d,'Color',[0.850980401039124 0.325490206480026 0.0980392172932625],'LineWidth', 2.0);
+    
+    
+    
+    set(gcf,'CurrentAxes',ax1)
+    ylabel(l1, 'FontName', 'Verdana', 'FontWeight', 'Bold','FontSize',8);
+    set(ax1, 'FontName', 'Verdana');
+    set(ax1, 'FontSize', font_size);
+    
+    % set(ax1,'ylim',([0,80]));
+    ax1.XTick = datenum(time_num(1:2:end));
+    datetick('x', 'dd mmm', 'keepticks') ;
+    set(ax1,'xlim',([t(1),t(end)]));
+    set(ax1,'XTickLabelRotation',53,'FontSize',6.5);
+    set(ax1,'Xcolor',[0 0.447058826684952 0.74117648601532]);
+    set(ax1,'Ycolor',[0 0.447058826684952 0.74117648601532]);
+    ylims=get(ax1,'ylim');
+    ylim(ax1,[0 ylims(2)]);
+    
+    
+    set(gcf,'CurrentAxes',ax2)
+    ylabel(l2, 'FontName', 'Verdana', 'FontWeight', 'Bold','FontSize',8);
+    set(ax2, 'FontName', 'Verdana');
+    set(ax2, 'FontSize', font_size);
+    % set(ax2,'xlim',([t(1)+offset_dec-1,t(end)+offset_dec-1]));
+    % set(ax2,'xlim',([t(1)+offset_dec-1,t(end)+offset_dec-1]));
+    % ax2.XTick = t(1:2:end)+offset_dec-1
+    % set(ax2,'ylim',([0,80]));
+    ax2.XTick = datenum(time_num(1:2:end))+offset_dec;
+    set(ax2,'xlim',([t(1)+offset_dec,t(end)+offset_dec]));
+    ax2.XTickLabel = datestr(datenum(time_num(1:2:end))+offset_dec);
+    datetick('x', 'dd mmm', 'keepticks') ;
+    
+    set(ax2,'XTickLabelRotation',53,'FontSize',6.5);
+    set(ax2,'Xcolor',[0.850980401039124 0.325490206480026 0.0980392172932625]);
+    set(ax2,'Ycolor',[0.850980401039124 0.325490206480026 0.0980392172932625]);
+    ylims=get(ax2,'ylim');
+    ylim(ax2,[0 ylims(2)]);
+    l=legend([a,b],l1,l2);
+    
+    set(l,'Location','northwest')
+    % overlap copyright info
+    datestr_now = datestr(now);
+    annotation(gcf,'textbox',[0.72342 0.00000 0.2381 0.04638],...
+        'String',{['Fonte: https://github.com/pcm-dpc']},...
+        'HorizontalAlignment','center',...
+        'FontSize',6,...
+        'FontName','Verdana',...
+        'FitBoxToText','off',...
+        'LineStyle','none',...
+        'Color',[0 0 0]);
+    
+    annotation(gcf,'textbox',...
+        [0.125695077559464 0.00165837479270315 0.238100000000001 0.04638],...
+        'String',{'https://covidguard.github.io/#covid-19-italia'},...
+        'LineStyle','none',...
+        'HorizontalAlignment','left',...
+        'FontSize',6,...
+        'FontName','Verdana',...
+        'FitBoxToText','off');
+    
+    annotation(gcf,'textbox',...
+        [0.328498902750058 0.735693681496092 0.537691794529686 0.0630182421227199],...
+        'String',['Offset: ', num2str(offset_dec), ' giorni'],...
+        'LineStyle','none',...
+        'HorizontalAlignment','right',...
+        'FontName','Verdana',...
+        'FitBoxToText','off');
+    
+    
+    
+    print(gcf, '-dpng', [WORKroot,'/slides/img/regioni/reg_correlazioneCasiDeceduti_v2_spline_', num2str(reg) ,'_', regione,'.PNG']);
+    close(gcf);
+    
     
     
     
@@ -4160,12 +4160,14 @@ data_casiPositivi=NaN(size(day_unique,1),1);
 data_deceduti=NaN(size(day_unique,1),1);
 data_terapie=NaN(size(day_unique,1),1);
 data_guariti=NaN(size(day_unique,1),1);
+data_ricoverati=NaN(size(day_unique,1),1);
 for k = 1: size(day_unique,1)
     index = find(strcmp(dataReg.data,day_unique(k)));
     data_casiPositivi(k)=sum(dataReg.totale_casi(index));
     data_deceduti(k)=sum(dataReg.deceduti(index));
     data_guariti(k)=sum(dataReg.dimessi_guariti(index));
     data_terapie(k)=sum(dataReg.terapia_intensiva(index));
+    data_ricoverati(k)=sum(dataReg.ricoverati_con_sintomi(index));
 end
 time_num = fix(datenum(day_unique));
 regione = 'Italia';
@@ -4490,6 +4492,146 @@ annotation(gcf,'textbox',...
 
 print(gcf, '-dpng', [WORKroot,'/slides/img/regioni/ita_correlazioneCasiDeceduti_v2_spline.PNG']);
 close(gcf);
+
+
+
+
+% 
+% 
+% 
+% %% rapporto deceduti/nuovi casi
+% figure;
+% id_f = gcf;
+% set(id_f, 'Name', 'Italia: rapporto deceduti/nuovi casi');
+% 
+% set(gcf,'NumberTitle','Off');
+% set(gcf,'Position',[26 79 967 603]);
+% grid on
+% hold on
+% 
+% y1 = diff(data_casiPositivi);
+% y2 = diff(data_deceduti);
+% y3 = diff(data_terapie);
+% y4 = diff(data_ricoverati);
+% 
+% 
+% a = plot(t,  y2./y1*100,'k');
+% b = plot(t,  y3./y1*100,'r');
+% c = plot(t,  y4./y1*100,'b');
+% 
+% [ax,a,b] = plotxx(t,y1,t,y2);
+% hold on
+% 
+% ax1 = ax(1);
+% ax2 = ax(2);
+% 
+% set(gcf,'CurrentAxes',ax1)
+% c=plot(t_1_1,a1_1,'-r');
+% 
+% set(gcf,'CurrentAxes',ax2)
+% d=plot(t_1_2,a1_2,'-b');
+% 
+% %
+% % a=plot(t,data_casiPositivi,'-ob','LineWidth', 2.0,'color',[0 0.200000002980232 0.600000023841858]); set(a,'markersize',6,'MarkerFaceColor',[0 0.447058826684952 0.74117648601532],'Color',[0 0.447058826684952 0.74117648601532]);
+% % b=plot(t-offset_dec,data_deceduti,'-ob','LineWidth', 2.0,'color',[1 0.200000002980232 0.600000023841858],'Parent',ax2); set(b,'markersize',6,'MarkerFaceColor',[0.850980401039124 0.325490206480026 0.0980392172932625],'Color',[0.850980401039124 0.325490206480026 0.0980392172932625]);
+% 
+% 
+% if ismac
+%     font_size = 9;
+% else
+%     font_size = 6.5;
+% end
+% 
+% set(a,'marker','o','markersize',4,'MarkerFaceColor',[0 0.447058826684952 0.74117648601532],'Color',[0 0.447058826684952 0.74117648601532],'LineWidth', 0.5);
+% set(b,'marker','o','markersize',4,'MarkerFaceColor',[0.850980401039124 0.325490206480026 0.0980392172932625],'Color',[0.850980401039124 0.325490206480026 0.0980392172932625],'LineWidth', 0.5);
+% set(c,'Color',[0 0.447058826684952 0.74117648601532],'LineWidth', 2.0);
+% set(d,'Color',[0.850980401039124 0.325490206480026 0.0980392172932625],'LineWidth', 2.0);
+% 
+% 
+% 
+% set(gcf,'CurrentAxes',ax1)
+% ylabel(l1, 'FontName', 'Verdana', 'FontWeight', 'Bold','FontSize',8);
+% set(ax1, 'FontName', 'Verdana');
+% set(ax1, 'FontSize', font_size);
+% 
+% % set(ax1,'ylim',([0,80]));
+% ax1.XTick = datenum(time_num(1:2:end));
+% datetick('x', 'dd mmm', 'keepticks') ;
+% set(ax1,'xlim',([t(1),t(end)]));
+% set(ax1,'XTickLabelRotation',53,'FontSize',6.5);
+% set(ax1,'Xcolor',[0 0.447058826684952 0.74117648601532]);
+% set(ax1,'Ycolor',[0 0.447058826684952 0.74117648601532]);
+% ylims=get(ax1,'ylim');
+% ylim(ax1,[0 ylims(2)]);
+% 
+% 
+% set(gcf,'CurrentAxes',ax2)
+% ylabel(l2, 'FontName', 'Verdana', 'FontWeight', 'Bold','FontSize',8);
+% set(ax2, 'FontName', 'Verdana');
+% set(ax2, 'FontSize', font_size);
+% % set(ax2,'xlim',([t(1)+offset_dec-1,t(end)+offset_dec-1]));
+% % set(ax2,'xlim',([t(1)+offset_dec-1,t(end)+offset_dec-1]));
+% % ax2.XTick = t(1:2:end)+offset_dec-1
+% % set(ax2,'ylim',([0,80]));
+% ax2.XTick = datenum(time_num(1:2:end))+offset_dec;
+% set(ax2,'xlim',([t(1)+offset_dec,t(end)+offset_dec]));
+% ax2.XTickLabel = datestr(datenum(time_num(1:2:end))+offset_dec);
+% datetick('x', 'dd mmm', 'keepticks') ;
+% 
+% set(ax2,'XTickLabelRotation',53,'FontSize',6.5);
+% set(ax2,'Xcolor',[0.850980401039124 0.325490206480026 0.0980392172932625]);
+% set(ax2,'Ycolor',[0.850980401039124 0.325490206480026 0.0980392172932625]);
+% ylims=get(ax2,'ylim');
+% ylim(ax2,[0 ylims(2)]);
+% l=legend([a,b],l1,l2);
+% 
+% set(l,'Location','northwest')
+% % overlap copyright info
+% datestr_now = datestr(now);
+% annotation(gcf,'textbox',[0.72342 0.00000 0.2381 0.04638],...
+%     'String',{['Fonte: https://github.com/pcm-dpc']},...
+%     'HorizontalAlignment','center',...
+%     'FontSize',6,...
+%     'FontName','Verdana',...
+%     'FitBoxToText','off',...
+%     'LineStyle','none',...
+%     'Color',[0 0 0]);
+% 
+% annotation(gcf,'textbox',...
+%     [0.125695077559464 0.00165837479270315 0.238100000000001 0.04638],...
+%     'String',{'https://covidguard.github.io/#covid-19-italia'},...
+%     'LineStyle','none',...
+%     'HorizontalAlignment','left',...
+%     'FontSize',6,...
+%     'FontName','Verdana',...
+%     'FitBoxToText','off');
+% 
+% print(gcf, '-dpng', [WORKroot,'/slides/img/regioni/ita_correlazioneCasiDeceduti_v2_spline.PNG']);
+% close(gcf);
+% 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
