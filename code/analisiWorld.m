900c78e296e99b03980b7526c94c9344591fd844
Andrea Gatti <gatti.andrea@gmail.com>
2021-03-05 17:54:39 +0000

    update 05/03

--- code/analisiWorld.m
+++ code/analisiWorld.m
@@ -44,67 +44,46 @@ filename1 = sprintf('%s/_json/world.csv',WORKroot);
     '%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s','delimiter',',','headerlines',1);
 
 
-world.dateNum=datenum(char(world.date));
-world.population=str2num(char(world.population));
+world.dateNum=datenum(world.date);
+world.population=str2double(world.population);
+world.total_cases_per_million=str2double(world.total_cases_per_million);
+world.total_deaths_per_million=str2double(world.total_deaths_per_million);
 
 
 % cut for minimum population
-pop_tresh = 5000000;
-
-
-indexOk = find(isfinite(world.dateNum) &world.population>pop_tresh);
-list_country =unique(world.countriesAndTerritories(indexOk));
+pop_tresh = 8000000;
+indexOk = find(isfinite(world.dateNum) &world.population>pop_tresh & ~strcmp(world.location,cellstr('European Union')) & ~strcmp(world.location,cellstr('North America'))& ~strcmp(world.location,cellstr('South America')));
+list_country =unique(world.location(indexOk));
 list_day=unique(world.dateNum(indexOk));
+
 
 worldData=struct;
 worldData.timeNum(:,1) = list_day;
-worldData.data=nan(numel(list_day), numel(list_country));
-worldData.dataWeight=nan(numel(list_day), numel(list_country));
-worldData.death=nan(numel(list_day), numel(list_country));
-worldData.deathWeight=nan(numel(list_day), numel(list_country));
-worldData.population=nan(1, numel(list_country));
+worldData.total_cases_per_million=nan(numel(list_day), numel(list_country));
+worldData.total_deaths_per_million=nan(numel(list_day), numel(list_country));
 
 for k = 1 : size(list_country,1)
-    idx_k=find(strcmp(world.countriesAndTerritories,list_country(k)));
-    worldData.population(1,k)=world.popData2018(idx_k(1));
+    idx_k=find(strcmp(world.location,list_country(k)));
+    worldData.population(1,k)=world.population(idx_k(1));
     
     for l = 1 : numel(idx_k)
         date_l=world.dateNum(idx_k(l));
         idx_l=find(list_day==date_l);
-        worldData.data(idx_l,k)=world.cases(idx_k(l));
-        worldData.dataWeight(idx_l,k)=world.cases(idx_k(l))/world.popData2018(idx_k(l));
-        worldData.death(idx_l,k)=world.deaths(idx_k(l));
-        worldData.deathWeight(idx_l,k)=world.deaths(idx_k(l))/world.popData2018(idx_k(l));        
-        
+        worldData.total_cases_per_million(idx_l,k)=world.total_cases_per_million(idx_k(l));
+        worldData.total_deaths_per_million(idx_l,k)=world.total_deaths_per_million(idx_k(l));
     end
 end
-temp=worldData.data;
-temp(isnan(temp))=0;
-worldData.dataSum = sum(temp);
-
-temp=worldData.dataWeight;
-temp(isnan(temp))=0;
-worldData.dataSumWeight = sum(temp);
 
 
-temp=worldData.death;
-temp(isnan(temp))=0;
-worldData.deathsSum = sum(temp);
-
-temp=worldData.deathWeight;
-temp(isnan(temp))=0;
-worldData.deathsSumWeight = sum(temp);
 
 
 %% TOTAL CASES
-n_lines=15;
+n_lines=20;
 
-[worstWeight,idx]=sort(worldData.dataSumWeight,'descend');
+[worstWeight,idx]=sort(worldData.total_cases_per_million(end,:),'descend');
 idx_country_worst=idx(1:n_lines);
 list_country(idx(1:n_lines))
 worldData.population(idx(1:n_lines))
-
-worldData.dataSumWeight(idx_country_worst)
 
 
 colors={[0 0.4470 0.7410],[0.8500 0.3250 0.0980],[0.9290 0.6940 0.1250],[0.4940 0.1840 0.5560],[0.4660 0.6740 0.1880],[0.3010 0.7450 0.9330],[0.6350 0.0780 0.1840]};
@@ -118,33 +97,32 @@ end
 
 %% confronto tra stati: casi totali allineato da 20 casi su 100.000
 
-aligmnent=10;
-
 date_s=list_day;
 h = figure;
 set(h,'NumberTitle','Off');
-title(sprintf('Nazioni con maggior numero di casi totali per abitante %s',datestr(date_s(end),'dd/mm/yyyy')));
-set(h,'Position',[26 79 967 603]);
+title(sprintf('Nazioni con maggior numero di casi totali ogni 100.000 abitanti (%s)',datestr(date_s(end),'dd/mm/yyyy')));
+set(h,'Position',[26 79 1632 886]);
 
 hold on; grid on; grid minor;
 % xlabel('Giorni dal caso 10/100.000 ab');
 ylabel('Casi totali per 100.000 abitanti')
 a=[];
 
+last_days=120;
 
+last_days=length(worldData.timeNum)-1;
 
 for reg = 1:size(idx_country_worst,2)
     reg
     regione = char(list_country(idx_country_worst(reg),1))
     
-    y=(worldData.dataWeight(:,idx_country_worst(reg))*100000);
+    y=(worldData.total_cases_per_million(:,idx_country_worst(reg))./10);
     y(isnan(y))=0;
-    y=cumsum(y);
+%     y=cumsum(y);
     
        try
-        aligmnent_reg=find(y>=aligmnent); aligmnent_reg=aligmnent_reg(1);
-        
-        a(reg)=plot([-aligmnent_reg:-aligmnent_reg+size(date_s,1)-1]', y,'LineWidth', 2.0, 'Color', colors{reg});
+
+        a(reg)=plot(worldData.timeNum(end-last_days:end), y(end-last_days:end),'LineWidth', 2.0, 'Color', colors{reg});
         
         idxx=find(~isnan(y));idxx=idxx(end);
         i = round(idxx/1)-1;
@@ -165,85 +143,34 @@ for reg = 1:size(idx_country_worst,2)
         
         
         %     text(i+1.2, y(i)+d, sprintf('%s (t0: %s)', regione, datestr(datenum(date_s(idx(1))),'dd-mmm')), 'rotation', a,'fontSize',7);
-        text(-aligmnent_reg+size(date_s,1)-1+0.5, y(i)+d, sprintf('%s', count), 'rotation', a,'fontSize',7);
+        text(worldData.timeNum(end)-1+0.5, y(i)+d, sprintf('%s', count), 'rotation', a,'fontSize',7, 'color',  colors{reg});
         
        catch
            fprintf('error on %d: %s\n', reg, char(list_country(idx(reg))));           
        end
 end
-
-
-
-
-% add custom country
-% customC_list = {'Brazil'; 'Russia';'China'};
-% customC_list = {'Italy';'Sweden'};
-
-% customC_list = {'Norway';'Sweden';'Finland'};
-worldData.dataWeight(isnan(worldData.dataWeight))=0;
-
-customC_list={''};
-for reg = 1:size(customC_list,1)    
-    regione = char(customC_list{reg});    
-    idx_cR = find(strcmp(list_country,regione));
-    
-    worldData.dataWeight(isnan(worldData.dataWeight(:,idx_cR)),idx_cR)=0;
-    y=cumsum(worldData.dataWeight(:,idx_cR)*100000);
-    
-    try
-        aligmnent_reg=find(y>=aligmnent); aligmnent_reg=aligmnent_reg(1);
-        aligmnent_reg=0
-        
-        a(reg)=plot([-aligmnent_reg:-aligmnent_reg+size(date_s,1)-1]', y,'LineWidth', 2.0, 'Color', colors{reg});
-        
-        idxx=find(~isnan(y));idxx=idxx(end);
-        i = round(idxx/1)-1;
-        
-        % Get the local slope
-        d = (y(i+1)-y(i-3))/4;
-        X = diff(get(gca, 'xlim'));
-        Y = diff(get(gca, 'ylim'));
-        p = pbaspect;
-        a = atan(d*p(2)*X/p(1)/Y)*180/pi;
-        if ~isfinite(a)
-            a=90;
-        end
-        
-        % Display the text
-        count = char(regione);
-        count=strrep(count,'_',' ');
-        
-        
-        %     text(i+1.2, y(i)+d, sprintf('%s (t0: %s)', regione, datestr(datenum(date_s(idx(1))),'dd-mmm')), 'rotation', a,'fontSize',7);
-        text(-aligmnent_reg+size(date_s,1)-1, y(i)+d, sprintf('%s', count), 'rotation', a,'fontSize',7);
-        
-       catch
-           fprintf('error on %d: %s\n', reg, char(list_country(idx(reg))));           
-       end
-end
-
-
 
 
 
 x_lim=get(gca,'xlim');
-xlim([-10,x_lim(2)]);
+xlim([worldData.timeNum(1),x_lim(2)]);
 % xlim([-10,120]);
 
 y_lim=get(gca,'ylim');
 ylim([10,y_lim(2)*1.1]);
 
-xlabel('giorni dal caso 10/100.000');
+xlabel('date');
 
-
-
+ax.XTick = (worldData.timeNum(1):5:x_lim(2));
+datetick('x', 'dd/mm/yyyy', 'keepticks') ;
+set(gca,'XTickLabelRotation',53,'FontSize',6.5);
 
 
 
 % overlap copyright info
 datestr_now = datestr(now);
 annotation(gcf,'textbox',[0.72342 0.00000 0.2381 0.04638],...
-    'String',{['Fonte: https://data.europa.eu']},...
+    'String',{['Fonte: https://raw.githubusercontent.com']},...
     'HorizontalAlignment','center',...
     'FontSize',6,...
     'FontName','Verdana',...
@@ -270,56 +197,43 @@ close(gcf);
 
 
 
+%% deceduti
 
-
-%% TOTAL DEATHS
-n_lines=20;
-
-[worstWeight,idx]=sort(worldData.deathsSumWeight,'descend');
+[worstWeight,idx]=sort(worldData.total_deaths_per_million(end,:),'descend');
 idx_country_worst=idx(1:n_lines);
 list_country(idx(1:n_lines))
 worldData.population(idx(1:n_lines))
 
-% worldData.dataSumWeight(idx_country_worst)
 
-
-colors={[0 0.4470 0.7410],[0.8500 0.3250 0.0980],[0.9290 0.6940 0.1250],[0.4940 0.1840 0.5560],[0.4660 0.6740 0.1880],[0.3010 0.7450 0.9330],[0.6350 0.0780 0.1840]};
-colors={};
-for k=1:n_lines
-    colors{k}=Cmap.getColor(k, n_lines);
-end
-
-
-
-
-%% confronto tra stati: morti allineato da 20 casi su 100.000
-
-aligmnent=2;
 
 date_s=list_day;
 h = figure;
 set(h,'NumberTitle','Off');
-title(sprintf('Nazioni con maggior numero di deceduti totali per abitante %s',datestr(date_s(end),'dd/mm/yyyy')));
-set(h,'Position',[26 79 967 603]);
+title(sprintf('Nazioni con maggior numero di deceduti ogni 100.000 abitanti (%s)',datestr(date_s(end),'dd/mm/yyyy')));
+set(h,'Position',[26 79 1632 886]);
 
 hold on; grid on; grid minor;
 % xlabel('Giorni dal caso 10/100.000 ab');
 ylabel('Deceduti per 100.000 abitanti')
 a=[];
 
+last_days=120;
 
+last_days=length(worldData.timeNum)-1;
 
 for reg = 1:size(idx_country_worst,2)
     reg
     regione = char(list_country(idx_country_worst(reg),1))
-    y=(worldData.deathWeight(:,idx_country_worst(reg))*100000);
-    y(isnan(y))=0;
-    y=cumsum(y);
+    
+    y=(worldData.total_deaths_per_million(:,idx_country_worst(reg))./10);
+
+    
+    if y(end)~=nan
+%     y=cumsum(y);
     
        try
-        aligmnent_reg=find(y>=aligmnent); aligmnent_reg=aligmnent_reg(1);
-        
-        a(reg)=plot([-aligmnent_reg:-aligmnent_reg+size(date_s,1)-1]', y,'LineWidth', 2.0, 'Color', colors{reg});
+
+        a(reg)=plot(worldData.timeNum(end-last_days:end), y(end-last_days:end),'LineWidth', 2.0, 'Color', colors{reg});
         
         idxx=find(~isnan(y));idxx=idxx(end);
         i = round(idxx/1)-1;
@@ -340,76 +254,35 @@ for reg = 1:size(idx_country_worst,2)
         
         
         %     text(i+1.2, y(i)+d, sprintf('%s (t0: %s)', regione, datestr(datenum(date_s(idx(1))),'dd-mmm')), 'rotation', a,'fontSize',7);
-        text(-aligmnent_reg+size(date_s,1)-1+0.5, y(i)+d, sprintf('%s', count), 'rotation', a,'fontSize',7);
+        text(worldData.timeNum(end)-1+0.5, y(i)+d, sprintf('%s', count), 'rotation', a,'fontSize',7, 'color',  colors{reg});
         
        catch
            fprintf('error on %d: %s\n', reg, char(list_country(idx(reg))));           
        end
+    end
 end
-
-
-
-% 
-% % add custom country
-% customC_list = {'Brazil'; 'Russia';'China'};
-
-% customC_list = {'Finland'; 'Sweden';'Norway';'Italy'};
-
-for reg = 1:size(customC_list,1)    
-    regione = char(customC_list{reg});    
-    idx_cR = find(strcmp(list_country,regione));
-    worldData.deathWeight(isnan(worldData.deathWeight(:,idx_cR)),idx_cR)=0;
-    y=cumsum(worldData.deathWeight(:,idx_cR)*100000);
-    
-       try
-        aligmnent_reg=find(y>=aligmnent); aligmnent_reg=aligmnent_reg(1);
-        
-        a(reg)=plot([-aligmnent_reg:-aligmnent_reg+size(date_s,1)-1]', y,'LineWidth', 2.0, 'Color', colors{reg});
-        
-        idxx=find(~isnan(y));idxx=idxx(end);
-        i = round(idxx/1)-1;
-        
-        % Get the local slope
-        d = (y(i+1)-y(i-3))/4;
-        X = diff(get(gca, 'xlim'));
-        Y = diff(get(gca, 'ylim'));
-        p = pbaspect;
-        a = atan(d*p(2)*X/p(1)/Y)*180/pi;
-        if ~isfinite(a)
-            a=90;
-        end
-        
-        % Display the text
-        count = char(regione);
-        count=strrep(count,'_',' ');
-        
-        
-        %     text(i+1.2, y(i)+d, sprintf('%s (t0: %s)', regione, datestr(datenum(date_s(idx(1))),'dd-mmm')), 'rotation', a,'fontSize',7);
-        text(-aligmnent_reg+size(date_s,1)-1+0.5, y(i)+d, sprintf('%s', count), 'rotation', a,'fontSize',7);
-        
-       catch
-           fprintf('error on %d: %s\n', reg, char(list_country(idx(reg))));           
-       end
-end
-
-
 
 
 
 x_lim=get(gca,'xlim');
-xlim([-10,x_lim(2)]);
+xlim([worldData.timeNum(1),x_lim(2)]);
+% xlim([-10,120]);
 
-xlabel('giorni dal decesso 2/100.000');
+y_lim=get(gca,'ylim');
+ylim([10,y_lim(2)*1.1]);
 
+xlabel('date');
 
-
+ax.XTick = (worldData.timeNum(1):5:x_lim(2));
+datetick('x', 'dd/mm/yyyy', 'keepticks') ;
+set(gca,'XTickLabelRotation',53,'FontSize',6.5);
 
 
 
 % overlap copyright info
 datestr_now = datestr(now);
 annotation(gcf,'textbox',[0.72342 0.00000 0.2381 0.04638],...
-    'String',{['Fonte: https://data.europa.eu']},...
+    'String',{['Fonte: https://raw.githubusercontent.com']},...
     'HorizontalAlignment','center',...
     'FontSize',6,...
     'FontName','Verdana',...
